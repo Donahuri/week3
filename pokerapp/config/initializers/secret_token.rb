c39b2fec38fc626f2016b03a2a1896c48cb3d19d
Richard H. Donahue <Rdonahue2014@kellogg.northwestern.edu>
2013-04-26 16:49:12 -0500

    Homework completed

---
+++ pokerapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokerapp::Application.config.secret_token = '23e2ab3e6249918249784c2e7a21abbc9e847ccf34f1a660c8085f6fa77434a96ced72c8af0ffca29ceb9a22f64f7ca44299e3abffce83c1e01bac5a5d9fde03'
